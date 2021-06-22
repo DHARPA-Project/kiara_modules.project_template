70d0ad0a9cfa5fc35128b82794ce352fe611eaf6
Markus Binsteiner <markus@frkl.io>
2021-06-22 09:43:52 +0200

    template: make the pipelines folder a module

---
+++ kiara_modules.{{ cookiecutter.project_slug }}/src/kiara_modules/{{ cookiecutter.project_slug }}/pipelines/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+"""Virtual module that is used as base for [PipelineModule][kiara.pipeline.module.PipelineModule] classes that are auto-generated
+from pipeline descriptions under this folder."""
